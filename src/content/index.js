1bdc5c3e58a477740e2b74ea22b94dd19f8799b3
xuzhiqiang <xuzhiqiang@qq.com>
2022-02-12 16:38:53 +0800

    fix: contentjs 按需导入组件

--- src/content/index.js
+++ src/content/index.js
@@ -1,17 +1,25 @@
 import Vue from 'vue'
 import content from './content.vue'
 import Mock from 'mockjs'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import ElementUI , {Button,Drawer,Table,TableColumn,Switch,Input} from 'element-ui';
+// // import 'element-ui/lib/theme-chalk/index.css';
+//按需导入,避免影响项目中的样式
+Vue.use(Button);
+Vue.use(Drawer);
+Vue.use(TableColumn);
+Vue.use(Table);
+Vue.use(Switch);
+Vue.use(Input);
+// Vue.use(ElementUI);
 
 var div = document.createElement('div')
-div.id = 'app'
+div.id = 'app1'
 var link  = document.createElement('link')
 link.rel='stylesheet'
 link.href="https://unpkg.com/element-ui/lib/theme-chalk/index.css"
-document.body.insertBefore(link,document.body.firstChild)
-document.body.insertBefore(div,document.body.firstChild)
+document.body.append(div)
+document.head.insertBefore(link,document.head.getElementsByTagName("link")[0])
+// document.body.insertBefore(div,document.body.firstChild)
 console.log({ window, document })
 window.abc1 = 130
 //可以访问window. document 但是添加不了属性
@@ -23,7 +31,7 @@ setTimeout(() => {
 }, 1000)
 
 new Vue({
-  el: '#app',
+  el: '#app1',
   render: (createElement) => {
     return createElement(content)
   },
